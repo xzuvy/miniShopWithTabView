0a8f56b68be63409b98ab986931916c1a6b0359f
Pete <xzuvy27@gmail.com>
2023-01-24 20:05:24 +0300

    Initial Commit

---
+++ miniShopWithTabView/miniShopWithTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  miniShopWithTabViewApp.swift
+//  miniShopWithTabView
+//
+//  Created by Peter Clarke on 24.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct miniShopWithTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
