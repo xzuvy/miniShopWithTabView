473f0feb9a25019bb04bf35a473cf9cc3cc798ad
Pete <xzuvy27@gmail.com>
2023-01-24 21:03:09 +0300

    Update ContentView.swift

--- miniShopWithTabView/ContentView.swift
+++ miniShopWithTabView/ContentView.swift
@@ -8,19 +8,96 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var positionOfTab = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        
+        TabView(selection: $positionOfTab){
+            
+            ProfileView(basketButton: $positionOfTab)
+                .tabItem{
+                    Image(systemName: "house.fill")
+                    Text("Профиль")
+                }
+                .tag(0)
+ 
+            CatalogView()
+            BasketView()
+ 
         }
-        .padding()
+        .accentColor(Color.pink)
+        .animation(Animation.spring(response: 0.7, blendDuration: 0.5))
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ProfileView: View {
+    
+    @Binding var basketButton : Int
+    
+    var body: some View {
+        ZStack{
+            Color.red.ignoresSafeArea(edges: .top)
+            Text("The first tab")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+            
+            Button(
+                action: {
+                    basketButton = 2
+                },
+                label: {
+                    Text("Перейти в корзину")
+                        .padding()
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .background(.green)
+                        .cornerRadius(20)
+                        .position(x : 185 , y : 500 )
+                    
+                })
+            
+        }
+        
+    }
+    
+}
+
+
+struct BasketView: View {
+    var body: some View {
+        ZStack{
+            Color.green.ignoresSafeArea(edges : .top)
+            Text("Корзина")
+        }
+        .badge(5)
+        .tabItem{
+            Image(systemName: "basket.fill")
+            Text("Корзина")
+            
+        }
+        .tag(2)
+    }
+}
+
+struct CatalogView: View {
+    var body: some View {
+        ZStack{
+            Color.pink.ignoresSafeArea(edges : .top)
+            Text("The second tab")
+            
+        }
+        .tabItem{
+            Image(systemName: "star.fill")
+            Text("Каталог")
+        }
+        .tag(1)
     }
 }
